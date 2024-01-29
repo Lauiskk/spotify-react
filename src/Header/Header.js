58d80379883d1b747ffafc4c129f88c46cf5d742
Lauiskk <vrluis157@gmail.com>
2024-01-29 17:10:36 -0300

    Training with React

---
+++ src/Header/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import './Header.css';
+import smallRight from '../assets/icons/small-right.png';
+import smallLeft from '../assets/icons/small-left.png';
+import search from '../assets/icons/search.png';
+
+
+const Header = () => {
+    return(
+        <nav className="header__navigation">
+        <div className="navigation">
+            <div className="arrows">
+                <button className="arrow-left">
+                    <img src={smallLeft} alt="Left Arrow"/>
+                </button>
+                <button className="arrow-right">
+                    <img src={smallRight} alt="Right Arrow"/>
+                </button>
+             </div>
+            <div className="header__search">
+                <img src={search} alt="Search Icon"/>
+                <input type="text" name="" id="search-input" maxlength="800" placeholder="O que você quer ouvir?"/>
+            </div>
+            <div className="header__login">
+                    <button className="subscribe">Inscreva-se</button>
+                    <button className="login">Entrar</button>
+            </div>
+            </div>
+        </nav>
+    )
+};
+
+export default Header;
